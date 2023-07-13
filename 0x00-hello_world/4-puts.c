6ea8cd80620f49182322f84376534430b417dcff
dev-soufiane <dev.sofyane@gmail.com>
2023-07-13 14:36:52 -0700

    Added 4-puts.c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point.
+ *
+ * Description: A C program that prints a message to the console.
+ *
+ * Return: It returns 0 to indicate successful program execution.
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
