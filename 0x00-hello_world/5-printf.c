191aa3018ef87950d76b699229b157a06fd7e150
dev-soufiane <dev.sofyane@gmail.com>
2023-07-13 14:48:53 -0700

    Added 5-printf.c program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point.
+ *
+ * Description: This program prints a message with printf.
+ *
+ * Return: It will return 0 to indicate successful program execution.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
