473ca27898c1e1064b83974dabca09c5867b0ac1
dev-soufiane <dev.sofyane@gmail.com>
2023-07-14 14:41:34 -0700

    Added 2-print_alphabet.c program

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point
+ *
+ * Description: This program prints the alphabet in lowercase
+ * using the putchar function() and a for loop.
+ * Return: 0 if the program runs successfully
+ */
+
+int main(void)
+{
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+		putchar(alpha[i]);
+	return (0);
+}
