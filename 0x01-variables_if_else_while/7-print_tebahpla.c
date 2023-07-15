9a123854cfb57622283e1840de687d719af0838e
dev-soufiane <dev.sofyane@gmail.com>
2023-07-14 18:20:08 -0700

    Added 7-print_tebahpla.c program

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point
+ *
+ * Description: This program prints the alphabet in lowercase
+ * in reverse using the putchar function() and a for loop.
+ * Return: 0 if the program runs successfully
+ */
+
+int main(void)
+{
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 26; i >= 0; i--)
+		putchar(alpha[i]);
+	putchar('\n');
+	return (0);
+}
