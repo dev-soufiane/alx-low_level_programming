20d2b8a47079e553f61936e0c95d0f6716909467
dev-soufiane <dev.sofyane@gmail.com>
2023-07-18 18:54:35 -0700

    Added 6-abs.c file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: The number to check
+ *
+ * Return: The absolute value of n
+ */
+
+int _abs(int n)
+{
+	if (n > 0 || n < 0)
+		return (n);
+	else
+		return (0);
+}
