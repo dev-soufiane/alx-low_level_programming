a44e68c21b0df6e68221d1ec77a5b7c6f3722b80
dev-soufiane <dev.sofyane@gmail.com>
2023-07-21 17:28:48 -0700

    Corrected if condition statement

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,7 +9,9 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	if (n > 0)
+	if (n <= 0)
+		_putchar('\n');
+	else
 	{
 		for (i = 1; i <= n; i++)
 		{
@@ -18,9 +20,5 @@ void print_diagonal(int n)
 		_putchar(92);
 		_putchar('\n');
 		}
-	}
-	else
-	{
-		_putchar('\n');
 	}
 }
