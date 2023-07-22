a832ca7b44300f7a271b6b8af8ecf2a6f119fd9c
dev-soufiane <dev.sofyane@gmail.com>
2023-07-21 18:34:11 -0700

    Updated if statements

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,11 +17,11 @@ int main(void)
 		{
 			printf("FizzBuzz");
 		}
-		else if (n % 3 == 0)
+		else if (n % 3 == 0 && !(n % 5 == 0))
 		{
 			printf("Fizz");
 		}
-		else if (n % 5 == 0)
+		else if (n % 5 == 0 && !(n % 3 == 0))
 		{
 			printf("Buzz");
 		}
