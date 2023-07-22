c3b4ef3d0aafcc19344e5981d5b25ed8111f066a
dev-soufiane <dev.sofyane@gmail.com>
2023-07-21 18:27:34 -0700

    Added 9-fizz_buzz.c file

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * main - prints numbers from 1 to 100
+ * replacing multiples of 3 with "Fizz"
+ * multiples of 5 with "Buzz", and multiples of both with "FizzBuzz"
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+	printf("\n");
+	return (0);
+}
