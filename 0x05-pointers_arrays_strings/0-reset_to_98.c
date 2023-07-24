ccc132c58343a1f8fc8d226993c7b36e255b4910
dev-soufiane <dev.sofyane@gmail.com>
2023-07-24 14:48:07 -0700

    Added 0-reset_to_98 file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates int pointer value to 98
+ * @n: pointer to an integer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,5 +5,6 @@
 
 /*Function prototype*/
 int _putchar(char c);
+void reset_to_98(int *n);
 
 #endif
