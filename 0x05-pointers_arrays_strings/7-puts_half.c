87dc49b4cd0844dcb23d818337b6cafecb7de9bb
dev-soufiane <dev.sofyane@gmail.com>
2023-07-25 16:49:04 -0700

    Declared i outside for loop

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,12 +8,12 @@
 
 void puts_half(char *str)
 {
-	int ln, n;
+	int ln, n, i;
 
 	ln = 0;
 	while (str[ln] != '\0')
 		ln++;
-	for (int i = ln / 2; i < ln ; i++)
+	for (i = ln / 2; i < ln ; i++)
 	{
 		n = (ln - 1) / 2;
 		if (n % 2 == 1)
