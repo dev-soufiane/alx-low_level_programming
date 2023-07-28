9cffb8927a0cc24f778c0680b4c8f36047d73cc5
dev-soufiane <dev.sofyane@gmail.com>
2023-07-28 14:46:27 -0700

    Changed while loop

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,16 +10,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, isEqual = 0;
+	isEqual = 0;
 
-	while (*(s1 + i) != '\0')
+	while (*s1)
 	{
-		if (*(s1 + i) != *s2)
+		if (*s1 != *s2)
 		{
 			isEqual = ((int)*s1 - 48) - ((int)*s2 - 48);
 			break;
 		}
-		i++;
+		s1++;
 		s2++;
 	}
 	return (isEqual);
