8222306323c65f2268fc2a2c672f6fac379069d6
dev-soufiane <dev.sofyane@gmail.com>
2023-08-11 19:17:24 -0700

    Update code

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,22 +12,14 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_ptr;
 	unsigned int i;
 	char *p;
 	char *s;
+	void *new_ptr;
 
 	if (new_size == old_size)
 		return (ptr);
 
-	if (ptr == NULL && new_size > 0)
-	{
-		new_ptr = malloc(new_size);
-		if (new_ptr == NULL)
-		{
-			exit(EXIT_FAILURE);
-		}
-		return (new_ptr);
 	}
 
 	if (ptr != NULL && new_size == 0)
@@ -40,7 +32,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_ptr == NULL)
 	{
-		exit(EXIT_FAILURE);
+		return (NULL);
 	}
 
 	p = (char *)new_ptr;
