3d5f18b356925384de24e357b74b6b85524b3248
dev-soufiane <dev.sofyane@gmail.com>
2023-08-17 11:05:33 -0700

    Change variable names in main function

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-	int a, b, result;
+	int num1, num2, result;
 	int (*func)(int, int);
 
 	if (argc != 4)
@@ -19,8 +19,8 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
 	func = get_op_func(argv[2]);
 
@@ -30,13 +30,13 @@ int main(int argc, char *argv[])
 		return (99);
 	}
 
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		return (100);
 	}
 
-	result = func(a, b);
+	result = func(num1, num2);
 
 	printf("%d\n", result);
 
