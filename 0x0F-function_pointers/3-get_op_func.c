42011f7cb05a689464b9f8d247db7cf9968133c0
dev-soufiane <dev.sofyane@gmail.com>
2023-08-17 10:54:53 -0700

    Update get_op_func function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -24,9 +24,9 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op != NULL)
 	{
-		if (*s == *(ops[i].op))
+		if (ops[i].op[0] == s[0] && s[1] == '\0')
 			return (ops[i].f);
-		i++;
+		++i;
 	}
 
 	return (NULL);
