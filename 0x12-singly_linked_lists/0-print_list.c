397a1424ab67ef455ef014c4663dd2ea2e9f1ea2
dev-soufiane <dev.sofyane@gmail.com>
2023-08-25 19:47:04 -0700

    Update task 0 function

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,18 +9,17 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t cnt = 0;
+	size_t cnt;
+
+	cnt = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
 			printf("[%d] %s\n", 0, "(nil)");
-		}
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-		}
+
 		h = h->next;
 		cnt++;
 	}
