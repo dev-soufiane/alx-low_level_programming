a3bfee66bf51fc2db1bacd2cfd8dc1f905dbffa9
dev-soufiane <dev.sofyane@gmail.com>
2023-09-01 13:57:56 -0700

    Update function code

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,26 +9,13 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int num;
-	int num_size = sizeof(unsigned long int) * 8;
-	int i;
+	unsigned long int i;
 
-	num = 1;
-	i = snum_ize - 1;
-
-	while (i >= 0)
+	for (i = 1UL << 63; i > 0; i = i >> 1)
 	{
-		if ((n >> i) & num)
-			break;
-		i--;
-	}
-
-	while (i >= 0)
-	{
-		if ((n >> i) & num)
+		if (n & i)
 			_putchar('1');
 		else
 			_putchar('0');
-		i--;
 	}
 }
