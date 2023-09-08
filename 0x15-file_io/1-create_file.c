ef9ae2c974d68c799fb9de185912892032fe3cbe
dev-soufiane <dev.sofyane@gmail.com>
2023-09-08 11:57:57 -0700

    Add _strlen function and update create_file code

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ * _strlen - Calculates the length of a string
+ * @str: The input string
+ *
+ * Return: The length of the string
+ */
+
+int _strlen(char *str)
+{
+	int i = 0;
+
+	if (str == NULL)
+		return (0);
+
+	while (str[i])
+		i++;
+
+	return (i);
+}
+
+/**
  * create_file - function that creates a file
  * @filename: pointer to the filename
  * @text_content: text to write to the file
@@ -12,26 +32,26 @@ int create_file(const char *filename, char *text_content)
 {
 	int f, w, i;
 
-	i = 0;
-
 	if (filename == NULL)
-		return (0);
+		return (-1);
 
 	f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (f == NULL)
+	if (f == -1)
 		return (-1);
 
 	if (text_content == NULL)
 		text_content = "";
 
-	while (text_content[i] != '\0')
-		i++;
+	i = _strlen(text_content);
 
 	w = write(f, text_content, i);
 
 	if (w == -1)
+	{
+		close(f);
 		return (-1);
+	}
 
 	close(f);
 
