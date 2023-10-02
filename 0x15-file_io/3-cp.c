f3c3fda7f79341c340d085a9531ccc914369290b
dev-soufiane <dev.sofyane@gmail.com>
2023-10-02 15:46:46 -0700

    Update 3-cp file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,75 +1,88 @@
 #include "main.h"
 
 /**
- * print_error - Print file close error.
- * @file_des: File descriptor.
+ * check_arguments - Check if the correct number of arguments is provided.
+ * @argc: Number of arguments.
  *
  * Return: None.
  */
 
-void print_error(int file_des)
+void check_arguments(int argc)
 {
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_des);
-	exit(100);
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 }
 
 /**
- * copy_file - Copy content from source to destination file.
- * @source_fd: Source file descriptor.
- * @dest_fd: Destination file descriptor.
+ * check_readability - Check if file_from exists and can be read.
+ * @check: Check result.
+ * @file_from: File_from name.
+ * @fd_from: File descriptor of file_from, or -1.
+ * @fd_to: File descriptor of file_to, or -1.
  *
- * Return: 0 on success, -1 on failure.
+ * Return: None.
  */
 
-int copy_file(int source_fd, int dest_fd)
+void check_readability(ssize_t check, char *file_from, int fd_from, int fd_to)
 {
-	char buffer[1024];
-	ssize_t bytes_read, bytes_written;
-
-	while ((bytes_read = read(source_fd, buffer, 1024)) > 0)
+	if (check == -1)
 	{
-		bytes_written = write(dest_fd, buffer, bytes_read);
-		if (bytes_written != bytes_read)
-		{
-			return (-1);
-		}
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
+		exit(98);
 	}
-	return ((bytes_read == 0) ? 0 : -1);
 }
 
 /**
- * open_source_and_destination - Open source and destination files.
- * @argv: Array containing program command line arguments.
- * @source_fd: Pointer to store source file descriptor.
- * @dest_fd: Pointer to store destination file descriptor.
+ * check_writability - Check if file_to can be created or written to.
+ * @check: Check result.
+ * @file_to: File_to name.
+ * @fd_from: File descriptor of file_from, or -1.
+ * @fd_to: File descriptor of file_to, or -1.
  *
- * Return: 0 on success, -1 on failure
+ * Return: None.
  */
 
-int open_source_and_destination(char *argv[], int *source_fd, int *dest_fd)
+void check_writability(ssize_t check, char *file_to, int fd_from, int fd_to)
 {
-	*source_fd = open(argv[1], O_RDONLY);
-
-	if (*source_fd == -1)
+	if (check == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		return (-1);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
+		exit(99);
 	}
-
-	*dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (*dest_fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(*source_fd);
-		return (-1);
-	}
-	return (0);
 }
 
 /**
- * main - Copy content from one file to another.
- * @argc: Number of command line arguments.
- * @argv: Array containing program command line arguments.
+ * check_closing - Check if file descriptors were closed properly.
+ * @check: Check result.
+ * @fd: File descriptor.
+ *
+ * Return: None.
+ */
+
+void check_closing(int check, int fd)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - Copy the content of a file to another file.
+ * @argc: Number of arguments passed.
+ * @argv: Array of pointers to the arguments.
  *
  * Return: 0 on success.
  */
@@ -77,25 +90,27 @@ int open_source_and_destination(char *argv[], int *source_fd, int *dest_fd)
 int main(int argc, char *argv[])
 {
 	int source_fd, dest_fd;
+	ssize_t read_bytes, written_bytes;
+	char buffer[1024];
+	mode_t permis;
 
-	if (argc != 3)
+	check_arguments(argc);
+	source_fd = open(argv[1], O_RDONLY);
+	check_readability((ssize_t)source_fd, argv[1], -1, -1);
+	permis = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, permis);
+	check_writability((ssize_t)dest_fd, argv[2], source_fd, -1);
+	read_bytes = 1024;
+	while (read_bytes == 1024)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		read_bytes = read(source_fd, buffer, 1024);
+		check_readability(read_bytes, argv[1], source_fd, dest_fd);
+		written_bytes = write(dest_fd, buffer, read_bytes);
+		if (written_bytes != read_bytes)
+			written_bytes = -1;
+		check_writability(written_bytes, argv[2], source_fd, dest_fd);
 	}
-	if (open_source_and_destination(argv, &source_fd, &dest_fd) == -1)
-		exit(98);
-	if (copy_file(source_fd, dest_fd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(source_fd);
-		close(dest_fd);
-		exit(99);
-	}
-	if (close(source_fd) < 0)
-		print_error(source_fd);
-	if (close(dest_fd) < 0)
-		print_error(dest_fd);
-
+	check_closing(close(dest_fd), dest_fd);
+	check_closing(close(source_fd), source_fd);
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,13 +7,15 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <stdio.h>
 
 /* Function prototypes */
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void print_error(int file_des);
-int copy_file(int source_fd, int dest_fd);
-int open_source_and_destination(char *argv[], int *source_fd, int *dest_fd);
+void check_arguments(int argc);
+void check_readability(ssize_t check, char *file_from, int fd_from, int fd_to);
+void check_writability(ssize_t check, char *file_to, int fd_from, int fd_to);
+void check_closing(int check, int fd);
 
 #endif
