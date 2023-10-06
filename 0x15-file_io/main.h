5aa9250f16d51bb7579a6871391e7cd70a513836
Soufiane <131982127+dev-soufiane@users.noreply.github.com>
2023-10-06 22:31:23 +0100

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,7 +8,6 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <stdio.h>
-#include <elf.h>
 
 /* Function prototypes */
 ssize_t read_textfile(const char *filename, size_t letters);
@@ -18,17 +17,5 @@ void check_arguments(int argc);
 void check_readability(ssize_t check, char *file_from, int fd_from, int fd_to);
 void check_writability(ssize_t check, char *file_to, int fd_from, int fd_to);
 void check_closing(int check, int fd);
-void validate_elf(unsigned char *ident);
-void print_elf_magic(unsigned char *ident);
-void print_elf_class(unsigned char *ident);
-void print_elf_data(unsigned char *ident);
-void print_elf_version(unsigned char *ident);
-void print_elf_osabi(unsigned char *ident);
-void print_elf_abi_version(unsigned char *ident);
-void print_elf_type(unsigned int type, unsigned char *ident);
-void print_elf_entry(unsigned long int entry, unsigned char *ident);
-void close_file(int fd);
-void print_elf_information(Elf64_Ehdr *header);
-void read_elf_header(int fd, Elf64_Ehdr *header);
 
 #endif
