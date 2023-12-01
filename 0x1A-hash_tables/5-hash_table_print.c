05b3749a9cad83e64c386e0ec3ca15f013c8c1d3
dev-soufiane <dev.sofyane@gmail.com>
2023-12-01 12:29:31 -0800

    Add 5-hash_table_print file

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Print all elements of the hash table.
+ * @ht: Pointer to the hash table
+ *
+ * Return: Nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+	int first = 1;
+
+	if (!ht || !ht->array)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			if (!first)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			first = 0;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
